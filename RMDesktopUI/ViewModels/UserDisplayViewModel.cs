e0a6c1f72f02a40a7d739684d4f74080dcdbff51
Sergey Ponomarev <sergey.ponomarev@rokolabs.com>
2020-01-15 17:04:29 +0400

    Add User Roles Manager

--- RMDesktopUI/ViewModels/UserDisplayViewModel.cs
+++ RMDesktopUI/ViewModels/UserDisplayViewModel.cs
@@ -18,6 +18,12 @@ namespace RMDesktopUI.ViewModels
         private readonly IWindowManager _window;
         private readonly IUserEndpoint _userEndpoint;
         private BindingList<UserModel> _users;
+        private UserModel _selectedUser;
+        private string _selectedUserName;
+        private BindingList<string> _userRoles = new BindingList<string>();
+        private BindingList<string> _availableRoles = new BindingList<string>();
+        private string _selectedUserRole;
+        private string _selecteAvailabledRole;
 
         public BindingList<UserModel> Users
         {
@@ -32,11 +38,91 @@ namespace RMDesktopUI.ViewModels
             }
         }
 
+        public UserModel SelectedUser
+        {
+            get { return _selectedUser; }
+            set
+            {
+                _selectedUser = value;
+                SelectedUserName = value.Email;
+                UserRoles.Clear();
+                UserRoles = new BindingList<string>(value.Roles.Select(x => x.Value).ToList());
+                LoadRoles();
+                NotifyOfPropertyChange(() => SelectedUser);
+            }
+        }
+
+        public string SelectedUserRole
+        {
+            get { return _selectedUserRole; }
+            set
+            {
+                _selectedUserRole = value;
+                NotifyOfPropertyChange(() => SelectedUserRole);
+            }
+        }
+
+        public string SelectedAvailableRole
+        {
+            get { return _selecteAvailabledRole; }
+            set
+            {
+                _selecteAvailabledRole = value;
+                NotifyOfPropertyChange(() => SelectedAvailableRole);
+            }
+        }
+
+        public string SelectedUserName
+        {
+            get { return _selectedUserName; }
+            set
+            {
+                _selectedUserName = value;
+                NotifyOfPropertyChange(() => SelectedUserName);
+            }
+        }
+
+        public BindingList<string> UserRoles
+        {
+            get { return _userRoles; }
+            set
+            {
+                _userRoles = value;
+                NotifyOfPropertyChange(() => UserRoles);
+            }
+        }
+
+        public BindingList<string> AvailableRoles
+        {
+            get { return _availableRoles; }
+            set
+            {
+                _availableRoles = value;
+                NotifyOfPropertyChange(() => AvailableRoles);
+            }
+        }
+
         public UserDisplayViewModel(StatusInfoViewModel status, IWindowManager window, IUserEndpoint userEndpoint)
         {
             _status = status;
             _window = window;
             _userEndpoint = userEndpoint;
+        }
+
+        public async void AddSelectedRole()
+        {
+            await _userEndpoint.AddUserToRole(SelectedUser.Id, SelectedAvailableRole);
+
+            UserRoles.Add(SelectedAvailableRole);
+            AvailableRoles.Remove(SelectedAvailableRole);
+        }
+
+        public async void RemoveSelectedRole()
+        {
+            await _userEndpoint.RemoveUserFromRole(SelectedUser.Id, SelectedUserRole);
+
+            AvailableRoles.Add(SelectedUserRole);
+            UserRoles.Remove(SelectedUserRole);
         }
 
         protected override async void OnViewLoaded(object view)
@@ -72,6 +158,19 @@ namespace RMDesktopUI.ViewModels
         {
             var userList = await _userEndpoint.GetAll();
             Users = new BindingList<UserModel>(userList);
+        }
+
+        private async Task LoadRoles()
+        {
+            var roles = await _userEndpoint.GetAllRoles();
+
+            foreach (var role in roles)
+            {
+                if (UserRoles.IndexOf(role.Value) < 0)
+                {
+                    AvailableRoles.Add(role.Value);
+                }
+            }
         }
     }
 }
